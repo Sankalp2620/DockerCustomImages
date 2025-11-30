12805291e6a51a946c901ce047fa0de093708660
Sankalp2620 <sankalpnandigama.307@gmail.com>
2025-11-30 21:26:21 +0530

    SpringappImage newrepo

---
+++ SpringappImage/src/main/java/com/docker/SpringappImage/SpringappImageApplication.java
@@ -0,0 +1,13 @@
+package com.docker.SpringappImage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappImageApplication.class, args);
+	}
+
+}
---
+++ SpringappImage/src/test/java/com/docker/SpringappImage/SpringappImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.SpringappImage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
