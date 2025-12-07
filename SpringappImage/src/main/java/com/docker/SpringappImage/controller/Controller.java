330751613a35662cd2fe39b671341fd009ffce17
Sankalp2620 <sankalpnandigama.307@gmail.com>
2025-12-07 11:53:06 +0530

    added the login end point

--- SpringappImage/src/main/java/com/docker/SpringappImage/controller/Controller.java
+++ SpringappImage/src/main/java/com/docker/SpringappImage/controller/Controller.java
@@ -1,13 +1,34 @@
 package com.docker.SpringappImage.controller;
 
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.docker.SpringappImage.dto.LoginRequest;
 
 @RestController
 public class Controller {
 	
 	@GetMapping("/hello")
-	public void getHello(){
-		System.out.println("hello");
+	public String getHello(){
+		return new String("Hello World");
+	}
+
+	/*
+	 * @PostMapping("/login") public ResponseEntity<String> LoginUser(
+	 * 
+	 * @RequestBody LoginRequest login) { return
+	 * ResponseEntity.ok("login successfull");
+	 * 
+	 * }
+	 */
+	
+	@GetMapping("/login")
+	public String showLoginMessage() {
+	    return "This endpoint requires POST — please submit username & password.";
 	}
 }
